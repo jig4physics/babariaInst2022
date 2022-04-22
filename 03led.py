4fc76b31f2874a822b14fbbf825807e669ffe3f8
Dr J <jig4physics@gmail.com>
2022-04-22 20:34:05 +0000

    Ready to role

--- 03led.py
+++ 03led.py
@@ -1,8 +1,30 @@
-from fastapi import FastAPI
+from flask import Flask
+import RPi.GPIO as GPIO
+import time
 
-app = FastAPI()
+button = 10
+led    = 8
 
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(led, GPIO.OUT)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}	
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return "Welcome to IOT"
+
+@app.route("/start")
+def start():
+    GPIO.output(led, True)
+    return "Light Start"
+
+@app.route("/stop")
+def stop():
+    GPIO.output(led, False)
+    return "Light Stop"
+
+if __name__ == '__main__':
+    app.run()
--- 05led.py
+++ 05led.py
@@ -1,27 +1,37 @@
-# -*- coding: utf-8 -*-
 import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO
+import time
 
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, rc):
-	print(“Connected with result code “+str(rc))
-# Subscribing in on_connect() means that if we lose the connection and
-# reconnect then subscriptions will be renewed.
-client.subscribe(“hello/world”)
+button = 10
+led    = 8
 
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-	print “Topic: “, msg.topic+’\nMessage: ‘+str(msg.payload)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(led, GPIO.OUT)
 
-client = mqtt.Client()
+# client of MQTT
+client = mqtt.Client("Jignesh")
+
+ # on connect
+def on_connect(clinet,userdata,flag, rc):
+    print("Connected with result code" +str(rc))
+
+# Suscribe topic 
+def on_message(client, userdata,msg):
+    print("Topic: ", msg.topic+'\nMessage: '+str(msg.payload))
+    command = str(msg.payload.decode("utf-8"))
+    print(command)
+    if command == "START":
+        GPIO.output(led, True)
+    else:
+        GPIO.output(led, False)
+
 client.on_connect = on_connect
 client.on_message = on_message
+client.connect("test.mosquitto.org", 1883)
+client.subscribe("my/led")
 
-client.connect("test.mosquitto.org", 1883, 60)
-
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+client.loop_start()
 
 
